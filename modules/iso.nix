2595c8d7cd265ad830896b435d56bec1cc4075ec
ofir <admin@hostme.co.il>
2026-03-26 00:53:07 +0200

    Update git repo url in flake.nix

---
+++ modules/iso.nix
@@ -0,0 +1,42 @@
+{
+  pkgs,
+  lib,
+  modulesPath,
+  ...
+}:
+{
+  imports = [
+    (modulesPath + "/installer/cd-dvd/installation-cd-graphical-calamares-plasma6.nix")
+    ./dev-setup.nix
+  ];
+
+  nixos-setup.graphical = true;
+
+  nix.settings = {
+    experimental-features = [ "nix-command" "flakes" ];
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  isoImage.isoName  = "nixos-ofir.iso";
+  isoImage.volumeID = "NIXOS_OFIR";
+  isoImage.squashfsCompression = "zstd -Xcompression-level 3";
+
+  boot.loader.timeout = 8;
+
+  services.openssh.enable = true;
+  users.users.root.initialPassword = "nixos";
+  users.users.nixos.initialPassword = "nixos";
+
+  services.earlyoom.enable = true;
+
+  environment.systemPackages = with pkgs; [
+    git
+    curl
+    wget
+    vim
+    pciutils
+    jq
+  ];
+}
+
