8effa223a24cc3b84a0b6d091ba7c27d59c50d59
ofir <admin@hostme.co.il>
2026-03-26 20:59:33 +0200

    fix: move grub device to host file

--- hosts/home-desktop.nix
+++ hosts/home-desktop.nix
@@ -47,7 +47,7 @@
 
   programs.firefox.enable = true;
 }
-services.displayManager.sddm.enable = true;
+  services.displayManager.sddm.enable = true;
   services.desktopManager.plasma6.enable = true;
 
   environment.plasma6.excludePackages = with (import <nixpkgs> {}).kdePackages; [
