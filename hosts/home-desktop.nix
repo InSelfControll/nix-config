6ab640a77911be69a6ac55f01e95928725f32505
ofir <admin@hostme.co.il>
2026-03-26 17:29:50 +0200

    fix: move grub device to host file

--- hosts/home-desktop.nix
+++ hosts/home-desktop.nix
@@ -1,4 +1,7 @@
 { ... }: {
   networking.hostName = "home-desktop";
   users.users.root.initialPassword = "nixos";
+
+  # Set this to whatever lsblk shows as your disk
+  boot.loader.grub.device = "/dev/vda";
 }
--- modules/base-system.nix
+++ modules/base-system.nix
@@ -1,6 +1,7 @@
-{ lib, ... }: {
+{ ... }: {
+  # Let Calamares/nixos-generate-config handle the bootloader device.
+  # Each host's hardware-configuration.nix already has the right value.
   boot.loader.grub.enable = true;
-  boot.loader.grub.device = lib.mkDefault "/dev/vda";
   boot.loader.grub.useOSProber = false;
 
   system.stateVersion = "25.11";
